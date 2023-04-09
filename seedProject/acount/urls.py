866a0cae70c908dbd90179bdf773f143e6f6cd77
a-im12 <ayumu.git@gmail.com>
2023-04-09 21:43:27 +0900

    userDBの作成

--- seedProject/acount/urls.py
+++ seedProject/acount/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'acount'
 
 urlpatterns = [
-
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('signup_success/', views.SignupSuccessView.as_view(), name='signup_success'),
 ]
--- seedProject/seedProject/settings.py
+++ seedProject/seedProject/settings.py
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
@@ -122,5 +122,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+AUTH_USER_MODEL = 'acount.CustomUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
