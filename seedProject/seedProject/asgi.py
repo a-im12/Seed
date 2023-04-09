82941635227bed2dfd2905b51deef7a748982f91
a-im12 <ayumu.git@gmail.com>
2023-04-09 11:58:07 +0900

    新規プロジェクトの作成🎉

--- seedProject/seedProject/asgi.py
+++ seedProject/seedProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for seed project.
+ASGI config for seedProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seedProject.settings')
 
 application = get_asgi_application()
