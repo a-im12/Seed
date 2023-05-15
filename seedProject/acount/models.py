06fd2d4a82d52f2d0906a85c8bbff2a5bbc67b7d
a-im12 <ayumu.git@gmail.com>
2023-05-15 11:30:47 +0900

    ADD:User.mission_point

--- seedProject/acount/models.py
+++ seedProject/acount/models.py
@@ -4,4 +4,5 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     is_general = models.BooleanField(null=True ,default=True)
     is_active = models.BooleanField(null=True ,default=False)
+    mission_point = models.IntegerField(null=False ,default=0)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
